183cc0b30f86ec928ca2ae4b12cca03d6b0ececc
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-18 12:02:49 +0530

    feat: add useState basic

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Table of Contents
+
+1. Vite setup
+2. 
---
+++ src/tutorial/01-useState/02-useState-basic.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const UseStateBasic = () => {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  
+  return (
+    <div>
+      <h1>useState basic</h1>
+      <h3>Button clicked {count} times</h3>
+      <button type="button" className="btn" onClick={increment}>
+        Click
+      </button>
+    </div>
+  );
+};
+
+export default UseStateBasic;
