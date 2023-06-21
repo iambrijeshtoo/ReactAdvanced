aa141fdcb37a4920a5d35ad6f43a0aeb195967fc
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-06-21 12:06:55 +0530

    refactor: use object in useState to update mutiple state

--- src/tutorial/01-useState/04-useState-object.jsx
+++ src/tutorial/01-useState/04-useState-object.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 
 const UseStateObjectExample = () => {
-    const [name, setName] = useState("Peter");
-    const [age  , setAge] = useState(24);
-    const [hobby, setHobby] = useState("Reading books");
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 23,
+    hobby: "Reading books",
+  });
 
-    const displayPerson = () => {
-        setName("John");
-        setAge(28);
-        setHobby("Playing games");
-    }
-    return <>
-        <h3>{name}</h3>
-        <h3>{age}</h3>
-        <h4>Enjoys to: {hobby}</h4>
-        <button className="btn" onClick={displayPerson}>Show John</button>
+  const displayPerson = () => {
+    setPerson({ name: "John", age: 28, hobby: "Playing games" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys to: {person.hobby}</h4>
+      <button className="btn" onClick={displayPerson}>
+        Show John
+      </button>
     </>
-}
+  );
+};
 export default UseStateObjectExample;
