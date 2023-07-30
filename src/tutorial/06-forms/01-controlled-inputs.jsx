29c32e17abafa427a7defdbab3f79036c9af2b84
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-07-30 14:52:45 +0530

    feat: add handle change function for updating input field state

--- src/tutorial/06-forms/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/01-controlled-inputs.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) => {
+    // console.log(e.target.name);
+    // console.log(e.target.email);
+    setName(e.target.name);
+    setEmail(e.target.email);
+  };
+
   return (
     <form className="form">
       <h4>Controlled Inputs</h4>
@@ -6,13 +18,25 @@ const ControlledInputs = () => {
         <label htmlFor="name" className="form-label">
           name
         </label>
-        <input type="text" id="name" className="form-input" />
+        <input
+          type="text"
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={handleChange}
+        />
       </div>
       <div className="form-row">
         <label htmlFor="email" className="form-label">
           email
         </label>
-        <input type="email" id="email" className="form-input" />
+        <input
+          type="email"
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit" className="btn btn-block">
         Submit
