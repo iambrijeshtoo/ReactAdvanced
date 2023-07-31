32621f4022cb811434f0b17bad3613ac75607286
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-07-31 16:39:10 +0530

    fix: name field does not take any input

--- src/tutorial/06-forms/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/01-controlled-inputs.jsx
@@ -4,9 +4,10 @@ const ControlledInputs = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  const handleChange = (e) => {
-    // console.log(e.target.name);
-    // console.log(e.target.email);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something
+    console.log(name, email);
   };
 
   return (
@@ -21,7 +22,7 @@ const ControlledInputs = () => {
           id="name"
           className="form-input"
           value={name}
-          onChange={(e) => setName(e.target.name)}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="form-row">
@@ -33,7 +34,7 @@ const ControlledInputs = () => {
           id="email"
           className="form-input"
           value={email}
-          onChange={(e) => setEmail(e.target.email)}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <button type="submit" className="btn btn-block">
