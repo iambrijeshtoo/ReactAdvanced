8b4aaa1c6d2747aeb1265e9fb5c59a51891be14c
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-08-05 14:01:47 +0530

    feat: add form for multiple inputs

--- src/tutorial/06-forms/03-multiple-inputs.jsx
+++ src/tutorial/06-forms/03-multiple-inputs.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 const MultipleInputs = () => {
-  return <h2>MultipleInputs</h2>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <div>
+      <form className="form">
+        <h2>MultipleInputs</h2>
+        {/* name */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input type="text" className="form-input" id="name" />
+        </div>
+        {/* Email */}
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input type="email" className="form-input" id="email" />
+        </div>
+        {/* Password */}
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
+            password
+          </label>
+          <input type="password" className="form-input" id="password" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default MultipleInputs;
