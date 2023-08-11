f5e9582e743f3853021cc56ab7ec698003b43881
iambrijeshtoo <chavdabrijeshkumar25@gmail.com>
2023-08-11 11:30:24 +0530

    switch to windows

--- src/tutorial/06-forms/04-other-inputs.jsx
+++ src/tutorial/06-forms/04-other-inputs.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 
+const frameworks = ["react", "angular", "vue", "svelte"];
+
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
+  const [framework, setFramework] = useState("react");
 
   const handleShipping = (e) => {
     console.log(e.target.checked);
     setShipping(e.target.checked);
   };
+
+  const handleframework = (e) => {};
+
   return (
     <div>
       <form className="form">
@@ -23,6 +29,11 @@ const OtherInputs = () => {
         </div>
         <div className="form-row" style={{ textAlign: "left" }}>
           <label htmlFor="framework">Framework</label>
+          <select name="framework" id="framework">
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>;
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           Submit
